295deb65db9f0712ac36ce81e8bea4199d45b688
Damien <damien.ramelet@protonmail.com>
2020-07-28 23:02:52 +0200

    Class input user. Add some tests

--- app/inputuser.py
+++ app/inputuser.py
@@ -1,4 +1,15 @@
 
 class InputUser:
     """Get input user."""
-    pass
+    
+    def __init__(self, raw_input):
+        """Create an input user object.
+
+        :param raw_input: A raw input from user
+        :type raw_input: str
+        """
+        self._raw_input = raw_input
+
+    @property
+    def raw_input(self):
+        return self._raw_input
